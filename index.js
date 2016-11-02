94379d3779096b1a408c90301253dd25c0550ef1
meronpang <blackout7@naver.com>
2016-11-02 14:56:16 +0900

    address end

--- index.js
+++ index.js
@@ -21,62 +21,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extened:true}));
 app.use(methodOverride("_method"));
 
-var contactSchema = mongoose.Schema({
-  name:{type:String, require:true, unique:true},
-  email:{type:String},
-  phone:{type:String}
-});
-var Contact = mongoose.model("contact",contactSchema);
-
-app.get("/", function(req, res){
- res.redirect("/contacts");
-});
-// Contacts - Index // 7
-app.get("/contacts", function(req, res){
- Contact.find({}, function(err, contacts){
-  if(err) return res.json(err);
-  res.render("contacts/index", {contacts:contacts});
-  });
-});
-// Contacts - New // 8
-app.get("/contacts/new", function(req, res){
- res.render("contacts/new");
-});
-// Contacts - create // 9
-app.post("/contacts", function(req, res){
- Contact.create(req.body, function(err, contact){
-  if(err) return res.json(err);
-  res.redirect("/contacts");
- });
-});
-app.get("/contacts/:id", function(req, res){
- Contact.findOne({_id:req.params.id}, function(err, contact){
-  if(err) return res.json(err);
-  res.render("contacts/show", {contact:contact});
- });
-});
-// Contacts - edit // 4
-app.get("/contacts/:id/edit", function(req, res){
- Contact.findOne({_id:req.params.id}, function(err, contact){
-  if(err) return res.json(err);
-  res.render("contacts/edit", {contact:contact});
- });
-});
-// Contacts - update // 5
-app.put("/contacts/:id", function(req, res){
- Contact.findOneAndUpdate({_id:req.params.id}, req.body, function(err, contact){
-  if(err) return res.json(err);
-  res.redirect("/contacts/"+req.params.id);
- });
-});
-// Contacts - destroy // 6
-app.delete("/contacts/:id", function(req, res){
- Contact.remove({_id:req.params.id}, function(err, contact){
-  if(err) return res.json(err);
-  res.redirect("/contacts");
- });
-});
-
+app.use("/", require("./routes/home"));
+app.use("/contacts", require("./routes/contacts"));
 
 app.listen(3000,function(){
     console.log("server ON!!");
